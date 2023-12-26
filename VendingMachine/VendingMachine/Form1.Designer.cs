d32397d710cda4a1ddfcc629c5c753371ac71320
Thanakorn Phiromkul <Thanakorn.ph@kkumail.com>
2023-12-26 19:18:12 +0700

    .

--- VendingMachine/VendingMachine/Form1.Designer.cs
+++ VendingMachine/VendingMachine/Form1.Designer.cs
@@ -41,9 +41,10 @@
             // BlackCoffee
             // 
             BlackCoffee.Image = (Image)resources.GetObject("BlackCoffee.Image");
-            BlackCoffee.Location = new Point(132, 32);
+            BlackCoffee.Location = new Point(185, 35);
+            BlackCoffee.Margin = new Padding(3, 2, 3, 2);
             BlackCoffee.Name = "BlackCoffee";
-            BlackCoffee.Size = new Size(177, 90);
+            BlackCoffee.Size = new Size(76, 68);
             BlackCoffee.TabIndex = 0;
             BlackCoffee.UseVisualStyleBackColor = true;
             BlackCoffee.Click += BlackCoffee_Click;
@@ -51,10 +52,11 @@
             // Mocha
             // 
             Mocha.Image = Properties.Resources.mocha;
-            Mocha.Location = new Point(133, 128);
+            Mocha.Location = new Point(185, 179);
+            Mocha.Margin = new Padding(3, 2, 3, 2);
             Mocha.Name = "Mocha";
             Mocha.RightToLeft = RightToLeft.No;
-            Mocha.Size = new Size(92, 90);
+            Mocha.Size = new Size(76, 68);
             Mocha.TabIndex = 1;
             Mocha.UseVisualStyleBackColor = true;
             Mocha.Click += Mocha_Click;
@@ -62,9 +64,10 @@
             // Latte
             // 
             Latte.Image = Properties.Resources.latte;
-            Latte.Location = new Point(132, 224);
+            Latte.Location = new Point(185, 107);
+            Latte.Margin = new Padding(3, 2, 3, 2);
             Latte.Name = "Latte";
-            Latte.Size = new Size(177, 90);
+            Latte.Size = new Size(76, 68);
             Latte.TabIndex = 2;
             Latte.UseVisualStyleBackColor = true;
             Latte.Click += Latte_Click;
@@ -72,9 +75,10 @@
             // Hotchocolate
             // 
             Hotchocolate.BackgroundImage = Properties.Resources.Hotchocolate;
-            Hotchocolate.Location = new Point(231, 128);
+            Hotchocolate.Location = new Point(185, 251);
+            Hotchocolate.Margin = new Padding(3, 2, 3, 2);
             Hotchocolate.Name = "Hotchocolate";
-            Hotchocolate.Size = new Size(78, 90);
+            Hotchocolate.Size = new Size(76, 68);
             Hotchocolate.TabIndex = 3;
             Hotchocolate.UseVisualStyleBackColor = true;
             Hotchocolate.Click += Hotchocolate_Click;
@@ -84,35 +88,40 @@
             pictureBox1.Anchor = AnchorStyles.Top;
             pictureBox1.BackColor = Color.LightGray;
             pictureBox1.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox1.Location = new Point(169, 360);
+            pictureBox1.Location = new Point(185, 377);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(126, 96);
+            pictureBox1.Size = new Size(110, 72);
             pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
             // ReStockButton
             // 
-            ReStockButton.Location = new Point(306, 574);
+            ReStockButton.BackColor = Color.LightCoral;
+            ReStockButton.Font = new Font("Showcard Gothic", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ReStockButton.Location = new Point(284, 535);
+            ReStockButton.Margin = new Padding(3, 2, 3, 2);
             ReStockButton.Name = "ReStockButton";
-            ReStockButton.Size = new Size(94, 83);
+            ReStockButton.Size = new Size(127, 130);
             ReStockButton.TabIndex = 5;
             ReStockButton.Text = "ปุ่มเติม";
-            ReStockButton.UseVisualStyleBackColor = true;
+            ReStockButton.UseVisualStyleBackColor = false;
             ReStockButton.Click += ReStockButton_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(412, 696);
+            BackgroundImage = Properties.Resources.TAOBIN1;
+            ClientSize = new Size(411, 676);
             Controls.Add(ReStockButton);
             Controls.Add(pictureBox1);
             Controls.Add(Hotchocolate);
             Controls.Add(Latte);
             Controls.Add(Mocha);
             Controls.Add(BlackCoffee);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
--- VendingMachine/VendingMachine/obj/Debug/net8.0-windows/VendingMachine.AssemblyInfo.cs
+++ VendingMachine/VendingMachine/obj/Debug/net8.0-windows/VendingMachine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VendingMachine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+007f56148e1eb335d7e807ba7d88a613d3bd679d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VendingMachine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VendingMachine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
